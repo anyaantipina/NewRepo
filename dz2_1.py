58568fb143618c63ace2f3056d4720067208580f
Anna Antipina <anya.antipina@gmail.com>
2019-03-24 19:11:17 +0300

    'Add' and 'Exit' buttons.
    The 'Add'-button adds a pair of a label and a button that changes colors of the text and background of label. The 'Exit'-button closes the program.

---
+++ dz2_1.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from random import choice
+
+COLORS = ("snow", "linen", "PeachPuff", "cornsilk", "ivory", "azure",
+          "white", "black", "gray", "navy", "blue", "cyan", "green",
+          "yellow", "khaki", "gold", "RosyBrown", "sienna", "peru",
+          "burlywood", "beige", "wheat", "tan", "brown", "salmon",
+          "orange", "coral", "tomato", "red", "pink", "maroon",
+          "violet", "plum", "orchid", "purple", "thistle")
+
+def change(event, label):
+    bg_txt = choice(COLORS)
+    fg_txt = choice(COLORS)
+    label.configure(bg = bg_txt, fg = fg_txt)
+
+i = 0
+
+def add(*args):
+    global i
+    i = i+1
+    root.columnconfigure(i, weight=1)
+    root.rowconfigure(i, weight=1)
+    Txt = Label(root, text="label")
+    Txt.grid(row=i, column=1, sticky=E+W+N)
+    Butt1 = Button(root, text="button")
+    Butt1.bind("<Button-1>", lambda event: change(event, Txt))
+    Butt1.grid(row=i, column=0, sticky=E+W+N)
+
+
+TKroot = Tk()
+TKroot.title("Hello")
+
+root = Frame(TKroot)
+root.place(relx=0, rely=0, relheight=1, relwidth=1)
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(0,weight=1)
+
+Butt = Button(root, text="Add")
+Butt.bind('<Button-1>', add)
+Butt.grid(row=0, column=0, sticky=E+W+N)
+Exit = Button(root, command=root.quit, text="Exit")
+Exit.grid(row=0, column=1, sticky=E+W+N)
+
+TKroot.mainloop()
+print("Done")
+root.destroy()
+TKroot.destroy()
